90776a8eb3a4acf9cbc9bd2621bf2db0404a7fb6
jacksons vm ubuntu <jdprice1995@gmail.com>
2020-12-05 18:44:41 -0600

    Building out production settings

--- leanai/prod_settings.py
+++ leanai/prod_settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = sec_vars.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -85,7 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'leanai',
-        'USER': 'jacksonprice',
+        'USER': 'jackson',
         'PASSWORD': sec_vars.DB_PW, 
         'HOST': '127.0.0.1',
         'PORT': '5432',
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_REDIRECT_URL = 'item-list'
 LOGIN_URL = 'login'
