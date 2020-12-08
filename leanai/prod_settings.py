e6a70da97b9eedda05846026d649dea21bf82bfd
jacksons vm ubuntu <jdprice1995@gmail.com>
2020-12-07 18:23:58 -0600

    add remote server to production hosts and reconfigured wsgi default settings

--- leanai/prod_settings.py
+++ leanai/prod_settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = sec_vars.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['192.168.1.200', '127.0.0.1', 'localhost']
 
 
 # Application definition
